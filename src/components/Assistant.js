e4dc21525f87ae616206d3570683dfce9d16e36a
steinbrennergit <steinbrennergit@gmail.com>
2018-12-12 21:12:57 -0700

    all buttons in place, refactor

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import DiceRoll from './DiceRoller';
+import InputSpinner from './InputSpinner';
 import './styles/Assistant.css';
 import './styles/bootstrap.min.css';
 import './styles/icon.css';
@@ -7,16 +7,77 @@ import './styles/icon.css';
 class Assistant extends Component {
   render() {
     return (
-      <div>
-        <DiceRoll numSides="20" />
-        <DiceRoll numSides="12" />
-        <DiceRoll numSides="10" />
-        <DiceRoll numSides="8" />
-        <DiceRoll numSides="6" />
-        <DiceRoll numSides="4" />
-        <DiceRoll numSides="3" />
-        <DiceRoll numSides="2" />
-        <DiceRoll numSides="%" />
+      <div className="container" style={{ backgroundColor: "#EEEEEE", paddingBottom: "15px" }}>
+        <table>
+          <tr>
+            <td>
+              <InputSpinner name="D20" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <InputSpinner name="D12" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <InputSpinner name="D10" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <InputSpinner name="D8" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <InputSpinner name="D6" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <InputSpinner name="D4" />
+            </td>
+            <td>
+              <InputSpinner name="+/-" />
+            </td>
+            <td>
+              <button className="roll-button">ROLL</button>
+            </td>
+          </tr>
+        </table>
+        <div className="output">
+        </div>
       </div>
     );
   }
