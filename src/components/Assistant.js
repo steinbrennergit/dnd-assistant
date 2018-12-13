bd241f97a81e95493c8f40a84bc6470a69a7fab3
steinbrennergit <steinbrennergit@gmail.com>
2018-12-12 20:40:26 -0700

    increment and decrement working, pass sides via props, rollers w/ state

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -8,7 +8,15 @@ class Assistant extends Component {
   render() {
     return (
       <div>
-        <DiceRoll />
+        <DiceRoll numSides="20" />
+        <DiceRoll numSides="12" />
+        <DiceRoll numSides="10" />
+        <DiceRoll numSides="8" />
+        <DiceRoll numSides="6" />
+        <DiceRoll numSides="4" />
+        <DiceRoll numSides="3" />
+        <DiceRoll numSides="2" />
+        <DiceRoll numSides="%" />
       </div>
     );
   }
--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -12,17 +12,41 @@ import './styles/DiceRoller.css';
 */
 
 class DiceRoller extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numDice: 0,
+            numSides: parseInt(props.numSides)
+        }
+        this.incrementDice = this.incrementDice.bind(this);
+        this.decrementDice = this.decrementDice.bind(this);
+    }
+
+    incrementDice() {
+        this.setState({
+            numDice: this.state.numDice + 1
+        })
+    }
+
+    decrementDice() {
+        if (this.state.numDice > 0) {
+            this.setState({
+                numDice: this.state.numDice - 1
+            })
+        }
+    }
+
     render() {
         return (
-            <div className="diceRoller">
-                <div className="flex-column">
-                    <button><i className="large material-icons">add</i></button>
-                    <button><i className="large material-icons">remove</i></button>
-                </div>
-                <div className="flex-column">
-                    <input style={{ width: '2rem', textAlign: 'center', height: '30px' }}></input>
-                    <div style={{ width: '32px', height: '30px', backgroundColor: 'blue', color: 'white', textAlign: 'center', lineHeight: '30px' }}>
-                    D20
+            <div className="flex-row">
+                <div className="dice-roller">
+                    <div className="flex-column">
+                        <button className="roller-box" onClick={this.incrementDice}><i className="large material-icons">add</i></button>
+                        <button className="roller-box" onClick={this.decrementDice}><i className="large material-icons">remove</i></button>
+                    </div>
+                    <div className="flex-column">
+                        <input className="roller-box" value={this.state.numDice}></input>
+                        <div className="roller-box dice-info">D{this.props.numSides}</div>
                     </div>
                 </div>
             </div>
